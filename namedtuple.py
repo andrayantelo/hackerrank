ca0e2bc1efa9fb21369d4cddb7132b044b451c87
andrayantelo <andrayantelo@gmail.com>
2018-09-25 23:37:11 +0000

    just trying to understand stdin and namedtuples

--- namedtuple.py
+++ namedtuple.py
@@ -1,8 +1,15 @@
 from collections import namedtuple
 import sys
 
+#ID, MARKS, CLASS and NAME
+
 n = int(input())
-cols = input().split()
-print(cols)
+cols = input()
+Student = namedtuple('Student', cols)
+# example: s = Student('Student', 'ID MARKS CLASS NAME')
+
 for line in sys.stdin:
-  line.split()
+  value_list = line.split()
+  print(value_list)
+  #value_list is an array like so: ['ana', '15', '1', '98']
+  
