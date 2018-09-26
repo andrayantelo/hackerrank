447ec27085d7bec2f7101d87496102fb1c9fd734
andrayantelo <andrayantelo@gmail.com>
2018-09-26 19:47:58 +0000

    got code down to 4 lines commented out test code

--- namedtuple.py
+++ namedtuple.py
@@ -3,13 +3,31 @@ import sys
 
 #ID, MARKS, CLASS and NAME
 
-n = int(input())
-cols = input()
-Student = namedtuple('Student', cols)
-# example: s = Student('Student', 'ID MARKS CLASS NAME')
+n, Student, sum_marks = int(input()), namedtuple('Student', input()), 0
+#n = 5
+#Student = namedtuple('Student', 'MARKS CLASS NAME ID')
+#sum_marks = 0
+#i = 0
 
-for line in sys.stdin:
-  value_list = line.split()
-  print(value_list)
-  #value_list is an array like so: ['ana', '15', '1', '98']
+#inf = open("input.txt", "r")
+#Student._make(line.split())
+#doc = inf.readlines()
+
+
+print(sum([int(Student._make(line.split()).MARKS) for line in sys.stdin])/n)
+
+#print(sum([int(Student._make(line.split()).MARKS) for line in doc)\n)
+
+
+
+#for line in sys.stdin:
+#  s = Student._make(line.split())
+#  sum_marks += int(s.MARKS)	
+  
+  
+#  i+= 1
+#  if i == 5:
+#	  break
+
+#print(sum_marks/n)
   
