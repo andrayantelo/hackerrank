6684500f6a8e8efdfcb934be71b31f5ba04896ea
andrayantelo <andrayantelo@gmail.com>
2018-10-25 23:43:02 +0000

    easy problem

--- python_problems/percentage.py
+++ python_problems/percentage.py
@@ -6,4 +6,30 @@
 # percentage marks obtained by that student, correct to two decimal places.
 
 # Input Format
-# 
+# The first line contains the integer n, the number of students. The next
+# n lines contains the name and marks obtained by that student separated by a space.
+# The final line contains the name of a particular student previously listed.
+
+# Constraints
+# 2 <= n <= 10
+# 0 <= Marks <= 100
+
+# Output Format
+# Print one line: The average of the marks obtained by the particular
+# student correct to 2 decimal places.
+
+if __name__ == '__main__':
+    n = int(input())
+    student_marks = {}
+    for _ in range(n):
+        name, *line = input().split()
+        scores = list(map(float, line))
+        student_marks[name] = scores
+    query_name = input()
+    
+    average = 0
+    for grade in student_marks[query_name]:
+        average += grade
+    average /= len(student_marks[query_name])
+    print('{:.2f}'.format(average))
+    
