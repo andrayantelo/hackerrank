f8840af3562f8aa8ebf16003aa007ddbd02c15ae
andrayantelo <andrayantelo@gmail.com>
2018-10-25 23:19:09 +0000

    changed folder name to avoid confusion

---
+++ python_problems/percentage.py
@@ -0,0 +1,9 @@
+# You have a record of n students. Each record contains the student's name,
+# and their percent marks in Maths, Physics and Chemistry. The marks can
+# be floating values. The user enters some integer n followed by the names
+# and marks for n students. You are required to save the record in a dictionary
+# data type. The user then enters a student's name. Output the average
+# percentage marks obtained by that student, correct to two decimal places.
+
+# Input Format
+# 
